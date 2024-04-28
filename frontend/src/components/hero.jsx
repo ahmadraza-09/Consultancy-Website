e1bd99cf06a650bf603e15043a096f43c0ecac36
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-04-28 17:00:02 +0530

    Update in hero.jsx

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigation, Autoplay, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useNavigate } from "react-router-dom";
 
 import '../css/hero.css'
 
@@ -12,6 +13,9 @@ import 'swiper/css/scrollbar';
 
 
 const Hero = () => {
+
+  const navigate = useNavigate();
+
   return (
     <>
     <div className='hero'>
@@ -39,66 +43,66 @@ const Hero = () => {
         <SwiperSlide>
           <div className='slider-card'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Recruitment Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our recruitment services help you find the right talent for your organization. Whether you're looking for permanent employees or temporary staff, we've got you covered. Our team of experts uses a comprehensive approach to match the skills and qualifications of candidates with your company's needs.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/recruitment`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slider-card slider-card2'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Apprenticeship Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our apprenticeship programs offer hands-on training and mentorship opportunities to individuals looking to kick-start their careers. Whether you're a recent graduate or switching fields, our programs provide valuable learning experiences to help you develop skills and gain practical knowledge in your chosen field.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/apprenticeship`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slider-card slider-card3'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Manpower Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our manpower solutions provide you with the workforce you need to meet your business objectives. Whether you need skilled labor for a specific project or temporary staff to cover peak seasons, we offer flexible staffing solutions to address your manpower requirements.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/manpower`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slider-card slider-card4'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Staffing Solution Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our staffing solutions provide comprehensive workforce management services to streamline your staffing processes and optimize your workforce. From recruitment to onboarding and beyond, we handle all aspects of staffing to ensure your organization operates efficiently and effectively.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/staffing-solution`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slider-card slider-card5'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Skill Development Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our skill development programs offer individuals the opportunity to enhance their skills and competencies to succeed in today's competitive job market. Whether you're looking to upgrade your existing skills or learn new ones, our programs provide practical training and support to help you achieve your career goals.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/skill-development`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slider-card slider-card6'>
             <div className="overlay">
-              <h2>We Provide Consultant Over India</h2>
+              <h2>We Provide Social Entrepreneurship Over India</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure veritatis est pariatur enim vel. Itaque rerum excepturi expedita? Inventore, corrupti. Natus omnis commodi nesciunt cum, vitae necessitatibus consectetur fugit dignissimos?
+                Our social entrepreneurship initiatives empower individuals and communities to create positive change through innovative solutions to social and environmental challenges. Whether you're a budding social entrepreneur or an established organization looking to make a difference, we provide support and resources to help you achieve your social impact goals.
               </p>
-              <button>Know More</button>
+              <button onClick={() => {navigate(`/service/social-entrepreneurship`)}}>Know More</button>
             </div>
           </div>
         </SwiperSlide>
